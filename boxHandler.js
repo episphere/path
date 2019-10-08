413824aa35854e48757b3aa3a0ff91dd77696d1b
PrafulB <prtsh32@gmail.com>
2019-10-08 13:10:45 -0400

    Box OAuth, rendering to canvas done

---
+++ boxHandler.js
@@ -0,0 +1,104 @@
+const box = async () => {
+  const client_id = "52zad6jrv5v52mn1hfy1vsjtr9jn5o1w"
+  const state = "sALTfOrSEcUrITy"
+  const redirect_uri = "http://localhost:8000"
+  const boxAuthEndpoint = encodeURI(`https://account.box.com/api/oauth2/authorize?response_type=code&client_id=${client_id}&state=${state}&redirect_uri=${redirect_uri}`)
+  const client_secret = "2rHTqzJumz8s9bAjmKMV83WHX1ooN4kT"
+  const boxAccessTokenEndpoint = "https://api.box.com/oauth2/token"
+  const boxUserEndpoint = "https://api.box.com/2.0/users/me"
+
+  document.getElementById("boxLoginBtn").onclick = () => window.location.replace(boxAuthEndpoint)
+
+  const isLoggedIn = async () => {
+    if (window.localStorage.box) {
+      let boxCreds = JSON.parse(window.localStorage.box)
+      if (boxCreds["box_access_token"] && boxCreds["box_token_expiry"]) {
+        if (boxCreds["box_token_expiry"] < Date.now()) {
+          boxCreds = await getNewAccessToken(boxCreds)
+        }
+        await getUserProfile(boxCreds)
+        return true
+      }
+    }
+    return false
+  }
+
+  const getNewAccessToken = async (boxCreds) => {
+    try {
+      const resp = await utils.request(boxAccessTokenEndpoint, {
+        'method': "POST",
+        'body': `grant_type=refresh_token&refresh_token=${boxCreds["box_refresh_token"]}&client_id=${client_id}&client_secret=${client_secret}`
+      })
+      return storeCredsToLS(resp)
+    } catch (err) {
+      console.log("ERROR REFRESHING BOX TOKEN!", err)
+      return {}
+    }
+  }
+
+  const storeCredsToLS = (boxCreds) => {
+    const expiry = (boxCreds["expires_in"] - 2 * 60) * 1000 + Date.now()
+    const newCreds = {
+      'box_access_token': boxCreds["access_token"],
+      'box_token_expiry': expiry,
+      'box_refresh_token': boxCreds["refresh_token"]
+    }
+    window.localStorage.box = JSON.stringify(newCreds)
+    return newCreds
+  }
+
+  const getUserProfile = async (boxCreds) => {
+    const {
+      id,
+      name,
+      login
+    } = await utils.request(boxUserEndpoint, {
+      'headers': {
+        'authorization': `Bearer ${boxCreds["box_access_token"]}`
+      }
+    })
+    window.localStorage.userId = id
+    window.localStorage.username = name
+    window.localStorage.email = login
+  }
+
+  if (await isLoggedIn()) {
+    document.getElementById("boxLoginBtn").style = "display: none"
+    document.getElementById("username").appendChild(document.createTextNode(`Welcome ${window.localStorage.username.split(" ")[0]}!`))
+    const boxPopup = new BoxSelect();
+    boxPopup.success(function (response) {
+      // document.getElementById("imageDiv").src = response[0].url
+      // const img = document.getElementById("selectedImage")
+      // img.onload = () => {
+      //   const canvas = document.createElement('canvas')
+      //   canvas.width = img.width
+      //   canvas.height = img.height
+      //   canvas.context = canvas.getContext('2d')
+      //   canvas.context.drawImage(img, 0, 0, img.width, img.height)
+      //   canvas.style = {
+      //     position: "absolute",
+      //     left: img.getBoundingClientRect().left
+      //   }
+      //   document.getElementById('canvasDiv').appendChild(canvas)
+      // }
+      console.log(response)
+    });
+    // Register a cancel callback handler
+    boxPopup.cancel(function () {
+      console.log("The user clicked cancel or closed the popup");
+    });
+    return
+  }
+  if (urlParams["code"]) {
+    try {
+      const resp = await utils.request(boxAccessTokenEndpoint, {
+        method: "POST",
+        body: `grant_type=authorization_code&code=${urlParams["code"]}&client_id=${client_id}&client_secret=${client_secret}`
+      })
+      storeCredsToLS(resp)
+      window.location.search = ""
+    } catch (err) {
+      console.log("ERROR LOGGING IN TO BOX!", err)
+    }
+  }
+}
--- path.js
+++ path.js
@@ -1,1 +1,51 @@
-console.log('path.js loaded')
+console.log("path.js loaded")
+
+const urlParams = {}
+window.location.search.slice(1).split('&').forEach(param => {
+  const [key, value] = param.split('=')
+  urlParams[key] = value
+})
+
+const defaultImg = "images/OFB_023_2_003_1_13_03.jpg"
+
+const utils = {
+  request: (url, opts) => fetch(url, opts).then(res => res.json()),
+}
+
+const path = async () => {
+  box()
+  path.root = document.getElementById("tmaPath")
+  path.imageDiv = document.getElementById("imageDiv")
+  path.loadCanvas()
+}
+
+path.loadCanvas = () => {
+  const fileInput = document.getElementById("imgInput")
+  const imgElement = document.createElement("img")
+  imgElement.setAttribute("id", "tmaImg")
+  imgElement.setAttribute("src", defaultImg)
+  imgElement.setAttribute("width", "500")
+  imgElement.setAttribute("height", "500")
+  path.imageDiv.appendChild(imgElement)
+
+  fileInput.onchange = ({
+    target: {
+      files
+    }
+  }) => {
+    document.getElementById("imgHeader").innerText = files[0].name
+    imgElement.setAttribute("src", URL.createObjectURL(files[0]))
+  }
+
+  const canvas = document.createElement("canvas")
+  canvas.setAttribute("id", "tmaCanvas")
+  imgElement.onload = () => {
+    canvas.setAttribute("width", imgElement.width)
+    canvas.setAttribute("height", imgElement.height)
+    const context = canvas.getContext('2d')
+    context.drawImage(imgElement, 0, 0)
+    path.imageDiv.appendChild(document.createElement("br"))
+    path.imageDiv.appendChild(canvas)
+
+  }
+}
